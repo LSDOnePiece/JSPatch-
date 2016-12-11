a29d518e36168b26feb6750386df5201527daefe
liushuai8887@126.com <liushuai8887@126.com>
2016-12-11 10:22:33 +0800

    创建工程

---
+++ JSPatch热更新/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSPatch热更新
+//
+//  Created by ls on 2016/12/11.
+//  Copyright © 2016年 OnePiece. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
